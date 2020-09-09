880161e9e9b5b53b58abc51bf5a63393e6b5c9c9
Valentijn Muijrers <valentijn.muijrers@hku.nl>
2020-09-10 00:05:57 +0200

    CodeConvention

---
+++ Basics/Assets/Scripts/CodeConvention.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeConvention
+{
+    //Class names with captial letters
+    public class CodeConvention
+    {
+        public float SomeProperty { get; set; }
+        public const float SOME_STATIC_FLOAT = 3.14f;
+
+        protected float _anotherFloat;
+
+        private float _someFloat;
+
+        public void SomeFunction()
+        {
+            bool someBool = true;
+            //Single line if-statements can be confined to one line
+            if (someBool) { return; }
+
+            if (!someBool)
+            {
+
+            }
+            else 
+            {
+                someBool = false;
+            }
+
+            switch(someBool)
+            {
+                case true: 
+                    break;
+                case false: 
+                    break;
+            }
+
+            //Spaces between operators
+            int result = 3 * 5;
+            result += 10;
+        }
+    }
+
+    //Interfaces start with capital I
+    public interface ISomeInterface
+    {
+
+    }
+}
