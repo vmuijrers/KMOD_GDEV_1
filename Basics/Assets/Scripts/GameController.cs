cb6fc13134b2baa0e46c6f5d1fd4d66c2e1bf136
Valentijn Muijrers <vmuijrers@gmail.com>
2020-09-17 17:36:54 +0200

    added GameController

--- Basics/Assets/Scripts/GameController.cs
+++ Basics/Assets/Scripts/GameController.cs
@@ -4,17 +4,80 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public class Player
+    {
+        private int actionIndex = 0;
+        private List<System.Action> allActions = new List<System.Action>();
+        public Player()
+        {
+            allActions.Add(Jump);
+            allActions.Add(Dash);
+            allActions.Add(Move);
+        }
+        
+        public void SelectedActionRight()
+        {
+            actionIndex = Mathf.Min(actionIndex + 1, allActions.Count - 1);
+        }
+
+        public void SelectedActionLeft()
+        {
+            actionIndex = Mathf.Max(actionIndex - 1, 0);
+        }
+
+        public void Jump() { }
+        public void Dash() { }
+        public void Move() { }
+
+        public void Use()
+        {
+            allActions[actionIndex].Invoke();
+        }
+    }
+
+    public class InputHandler
+    {
+        public event System.Action OnXButtonPressed;
+        public event System.Action OnLeftButtonPressed;
+        public event System.Action OnRightButtonPressed;
+        public void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                OnXButtonPressed?.Invoke();
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                OnLeftButtonPressed?.Invoke();
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                OnRightButtonPressed?.Invoke();
+            }
+        }
+    }
+
+    private Player player;
+    private InputHandler input = new InputHandler();
+
     // Start is called before the first frame update
     void Start()
     {
-
+        player = new Player();
+        //Context
+        input.OnXButtonPressed += player.Jump;
+        input.OnXButtonPressed += player.Use;
+        input.OnLeftButtonPressed += player.SelectedActionLeft;
+        input.OnRightButtonPressed += player.SelectedActionRight;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
 
@@ -87,46 +150,67 @@ public class GameController : MonoBehaviour
 //}
 ////Usage: SomeStaticMethodClass.SomeStaticFunction()
 
-//public interface IState
-//{
-//    void OnEnter();
-//    void OnUpdate();
-//    void OnExit();
-//}
+namespace StateMachine
+{
 
-//public class StateMachine
-//{
-//    private IState currentState;
-//    private Dictionary<System.Type, IState> states = new Dictionary<System.Type, IState>();
 
-//    public void OnStart(){
-//        AddState(new IdleState(this));
-//        AddState(new AttackState(this));
-//    }
+    public class Test : MonoBehaviour
+    {
+        private StateMachine stateMachine;
+        private void Awake()
+        {
+            stateMachine = new StateMachine();
 
-//    public void OnUpdate(){
-//        currentState?.OnUpdate();
-//    }
+            //stateMachine.AddState(new IdleState(stateMachine));
+            //stateMachine.AddState(new AttackState(stateMachine));
+        }
 
-//    public void SwitchState(System.Type type){
-//        currentState?.OnExit();
-//        currentState = states[type];
-//        currentState?.OnEnter();
-//    }
+        private void Update()
+        {
+            stateMachine.OnUpdate();
+        }
+    }
 
-//    public void AddState(IState state){
-//        states.Add(state.GetType(), state);
-//    }
-//}
 
-//public abstract class State : IState
-//{
-//    public State(StateMachine owner)
-//    {
-//        this.owner = owner;
-//    }
-//    protected StateMachine owner;
-//    public abstract void OnEnter();
-//    public abstract void OnUpdate();
-//    public abstract void OnExit();
-//}
+    public interface IState
+    {
+        void OnEnter();
+        void OnUpdate();
+        void OnExit();
+    }
+
+    public class StateMachine
+    {
+        private IState currentState;
+        private Dictionary<System.Type, IState> states = new Dictionary<System.Type, IState>();
+
+        public void OnUpdate()
+        {
+            currentState?.OnUpdate();
+        }
+
+        public void SwitchState(System.Type type)
+        {
+            currentState?.OnExit();
+            currentState = states[type];
+            currentState?.OnEnter();
+        }
+
+        public void AddState(IState state)
+        {
+            states.Add(state.GetType(), state);
+        }
+    }
+
+    public abstract class State : IState
+    {
+        public State(StateMachine owner)
+        {
+            this.owner = owner;
+        }
+        protected StateMachine owner;
+        public abstract void OnEnter();
+        public abstract void OnUpdate();
+        public abstract void OnExit();
+    }
+}
